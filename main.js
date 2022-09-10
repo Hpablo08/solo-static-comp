665aaacf672d39ef361f11eff3e0cffeda832bea
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-09-10 13:05:18 -0600

    Refactored code

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 var logInButton  = document.querySelector('.logInButton')
 var userNameInput = document.querySelector('.user-name')
-var sideBarButton = document.querySelector('.side-bar-button')
 var nameDisplay = document.querySelector('.display-name')
-var leftNav = document.querySelector('.left-nav')
 
 logInButton.addEventListener('click', displayName)
-
-
 
 function displayName() {
   console.log('hello')
@@ -14,13 +10,11 @@ function displayName() {
 
 }
 
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
 }
 
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
